9bed0067f0cb29ea23da937161f6f0f9c2edd9ff
Nico Lerchl <nico@lerchl.one>
2022-10-02 14:58:52 +0200

    close message queue

--- myfind/main.cpp
+++ myfind/main.cpp
@@ -23,6 +23,19 @@ typedef std::function<std::string(std::string)> stringMapper;
 void signalHandler(int sig);
 
 /**
+ * Parses arguments.
+ * @param arc Argument count
+ * @param argv Argument values
+ * @param optionCounterR reference to recursive option counter
+ * @param optionCounterI reference to ignore case option counter
+ * @param path reference to path string
+ * @param filenames reference to filename list
+ */
+void parseArguments(int argc, char* argv[],
+                    unsigned short& optionCounterR, unsigned short& optionCounterI,
+                    std::string& path, std::list<std::string>& filenames);
+
+/**
  * Iterate over directory with certain path searching for filename.
  * @param path searchpath
  * @param filename name of the searched file
@@ -42,15 +55,74 @@ typedef struct {
 	char mText[MAX_DATA];
 } message_t;
 
+/**
+ * 
+ */
 int main(int argc, char* argv[]) {
     // set signal handler for zombie processes
     signal(SIGCHLD, signalHandler);
 
-    int c;
     unsigned short optionCounterR = 0;
     unsigned short optionCounterI = 0;
     std::string path;
     std::list<std::string> filenames;
+
+    parseArguments(argc, argv, optionCounterR, optionCounterI, path, filenames);
+
+    // use lowercase mapper if i (ignore case) flag has been set
+    stringMapper stringMapper = optionCounterI ?
+            [] (std::string s) { std::transform(s.begin(), s.end(), s.begin(), ::tolower); return s; } :
+            [] (std::string s) { return s; };
+
+    int pid = getpid();
+
+    int msgid;
+    if ((msgid = msgget(KEY, PERM | IPC_CREAT | IPC_EXCL)) == -1) {
+        std::cerr << "Was not able to create message queue" << std::endl;
+        exit(4);
+    }
+
+    for (const auto& filename : filenames) {
+        if (pid == getpid() && fork() == 0) {
+            iterateDirectory(path, filename, optionCounterR, stringMapper);
+            return 0;
+        }
+    }
+
+    message_t message = { .mType = 0 };
+    while (message.mType == 0) {
+        if (msgrcv(msgid, &message, sizeof(message) - sizeof(long), 0 , 0) == -1) {
+            // last message recieved
+            break;
+        } else {
+            std::cout << message.mText << std::endl;
+            message = { .mType = 0 };
+        }
+    }
+
+    if (msgctl(msgid, IPC_RMID, NULL) == -1) {
+        std::cerr << "Could not remove message queue" << std::endl;
+        exit(6);
+    } else {
+        std::cout << "Message queue removed" << std::endl;
+    }
+
+    return 0;
+}
+
+void signalHandler(int sig) {
+    pid_t pid;
+    int ret;
+
+    while ((pid = waitpid(-1, &ret, WNOHANG)) > 0) {
+        std::cout << "Child with pid " << std::to_string(pid) << " collected" << std::endl;
+    }
+}
+
+void parseArguments(int argc, char* argv[],
+                    unsigned short& optionCounterR, unsigned short& optionCounterI,
+                    std::string& path, std::list<std::string>& filenames) {
+    int c;
 
     while ((c = getopt(argc, argv, "Ri")) != EOF) {
         switch (c) {
@@ -90,46 +162,6 @@ int main(int argc, char* argv[]) {
         std::cerr << "Searchpath is not a directory" << std::endl;
         exit(3);
     }
-
-    // use lowercase mapper if i (ignore case) flag has been set
-    stringMapper stringMapper = optionCounterI ?
-            [] (std::string s) { std::transform(s.begin(), s.end(), s.begin(), ::tolower); return s; } :
-            [] (std::string s) { return s; };
-
-    int pid = getpid();
-
-    int msgid;
-    if ((msgid = msgget(KEY, PERM | IPC_CREAT | IPC_EXCL)) == -1) {
-        std::cerr << "Was not able to create message queue" << std::endl;
-        exit(4);
-    }
-
-    for (const auto& filename : filenames) {
-        if (pid == getpid() && fork() == 0) {
-            iterateDirectory(path, filename, optionCounterR, stringMapper);
-            return 0;
-        }
-    }
-
-    message_t message;
-    while (1) {
-        if (msgrcv(msgid, &message, sizeof(message) - sizeof(long), 0 , 0) == -1) {
-            std::cerr << "Could not recieve message" << std::endl;
-            exit(5);
-        }
-        std::cout << message.mText << std::endl;
-    }
-
-    return 0;
-}
-
-void signalHandler(int sig) {
-    pid_t pid;
-    int ret;
-
-    while ((pid = waitpid(-1, &ret, WNOHANG)) > 0) {
-        std::cout << "Child with pid " << std::to_string(pid) << " collected" << std::endl;
-    }
 }
 
 void iterateDirectory(const fs::path& searchPath, std::string filename, bool recursive, stringMapper stringMapper) {
@@ -156,7 +188,7 @@ void sendFileFoundMessage(const fs::path& filePath) {
     }
 
     message_t message = { .mType = 1 };
-    std::string text = pid + ": " + filePath.filename().generic_string() + ": " + filePath.generic_string();
+    std::string text = std::to_string(pid) + ": " + filePath.filename().generic_string() + ": " + filePath.generic_string();
     text.copy(message.mText, MAX_DATA);
 
     if (msgsnd(msgid, &message, sizeof(message) - sizeof(long), 0) == -1) {
